8e6a787682a232f47c690e69e8a43a48cc7f3bcd
Handi Dwi Cahyo <87643319+Wisle25@users.noreply.github.com>
2022-09-20 20:58:18 +0700

    Added Pola 6

---
+++ pola6.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    // Jika n adalah genap, ubah n ke bentuk ganjil di bawahnya
+    if (n % 2 == 0)
+        n -= 1;
+
+    // Aku buatnya dengan konsep setengah...jadi setengah atas dan setengah bawah
+    int half = n / 2;
+
+    for (int i = 0; i < half; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            printf("  ");
+        }
+
+        for (int j = i; j < half; j++)
+        {
+            if (j > i)
+                printf("  ");
+            else
+                printf("* ");
+        }
+
+        for (int j = half; j >= i; j--)
+        {
+            if (j > i)
+                printf("  ");
+            else
+                printf("* ");
+        }
+        printf("\n");
+    }
+
+    for (int i = 1; i <= half + 1; i++)
+    {
+        for (int j = i; j <= half; j++)
+            printf("  ");
+
+        for (int j = i; j > 1; j--)
+        {
+            if (j >= i)
+                printf("* ");
+            else
+                printf("  ");
+        }
+
+        for (int j = 1; j <= i; j++)
+        {
+            if (j == i)
+                printf("* ");
+            else
+                printf("  ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
